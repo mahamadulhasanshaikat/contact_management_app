d2ce77fc8bff45aca58535a7ea46559d9ee22073
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-08-16 01:12:01 +0600

    update

---
+++ lib/views/contact_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyContactPage extends StatefulWidget {
+  const MyContactPage({super.key});
+
+  @override
+  State<MyContactPage> createState() => _MyContactPageState();
+}
+
+class _MyContactPageState extends State<MyContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+
+    );
+  }
+}
