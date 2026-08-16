8757fc285b9de6f71b0631537e9ee2c2bc6ae733
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-08-16 17:17:43 +0600

    update

--- lib/features/add_contact/add_contact_page.dart
+++ lib/features/add_contact/add_contact_page.dart
@@ -1,3 +1,4 @@
+import 'package:contact_management_app/app/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AddContactPage extends StatefulWidget {
@@ -10,6 +11,26 @@ class AddContactPage extends StatefulWidget {
 class _AddContactPageState extends State<AddContactPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: Text("Add Contact", style: TextStyle(color: Colors.white)),
+        backgroundColor: AppColors.appbar,
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.check_circle),
+            iconSize: 35,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: SafeArea(child: Column(children: [
+
+        
+      ],)),
+    );
   }
 }
